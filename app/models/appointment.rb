27a40c059386abe65dfeb3c69076aa8cca094e36
Chris Laverdiere <cmlaverdiere@gmail.com>
2013-09-28 17:42:47 +0000

    Font changed to Google's OpenSans temporarilly, email validation attempt

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,14 @@
+# class EmailValidator < ActiveModel::EachValidator
+#   def validate_each(record, attribute, value)
+#     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+#       record.errors[attribute] << " is not a valid email"
+#     end
+#   end
+# end
+
 class Appointment < ActiveRecord::Base
-  validates :email, :name, :phone, :time, presence: true
+  validates :email, :name, :phone, :time, :reason, presence: true
+  # validates :email, presence: true, email: true
 
   def first_name
     @first_name = name.split(' ')[0]
