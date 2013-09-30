6d24749891c2b0f3f72ace2a7c4315ec95666481
Chris Laverdiere <cmlaverdiere@gmail.com>
2013-09-30 12:36:20 +0000

    Fixed missing auth in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with name: 'scott', password: 'bym2JSt95Hl1', only: [:index]
+  # http_basic_authenticate_with name: 'scott', password: 'bym2JSt95Hl1', only: [:index]
+  http_basic_authenticate_with name: APP_CONFIG['username'], 
+                           password: APP_CONFIG['password'], only: [:index]
 
   def index
   end
