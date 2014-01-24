c66555cacc5e04e46e5998493d03d625af11b7cd
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-01-24 18:13:59 -0500

    Try alternative config file setting

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Load config.yml
-APP_CONFIG = YAML.load_file("config/config.yml")
+APP_CONFIG = YAML.load_file(File.join(__dir__, 'config.yml'))
 
 # Initialize the Rails application.
 SlMobileMech::Application.initialize!
