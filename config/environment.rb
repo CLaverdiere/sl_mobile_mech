5023cb0d46e77cfb874a4e11e290839096f80e8e
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-01-24 20:13:22 -0500

    Move to figaro config

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Load config.yml
-begin
-  APP_CONFIG = YAML.load_file("config/config.yml")
-rescue
-  APP_CONFIG = { :username => 'scott', :password => 'onheroku!<>' }
-end
-
 # Initialize the Rails application.
 SlMobileMech::Application.initialize!
