e3b8ed41a04ac52ca45d98c7bab77c6819a7984c
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-01-24 20:14:44 -0500

    Remove instance of APP_CONFIG in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
   # http_basic_authenticate_with name: 'scott', password: 'bym2JSt95Hl1', only: [:index]
-  http_basic_authenticate_with name: APP_CONFIG['username'], 
-                           password: APP_CONFIG['password'], only: [:index]
+  http_basic_authenticate_with name: ENV['username'], 
+                           password: ENV['password'], only: [:index]
 
   def index
   end
