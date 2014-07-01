a187bfed40c41dbadc6122460bee022ea6838aba
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-07-01 19:00:49 -0400

    Add production email settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,18 @@ SlMobileMech::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'http://www.cmlaverdiere.net',
+    user_name: ENV['mail_email'],
+    password: ENV['mail_pass'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
